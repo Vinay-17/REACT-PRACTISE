2b319d8de8d059e07be54fdbb2e9bd4606dfd53b
Vinay-17 <vinayveerapaneni068@gmail.com>
2025-11-20 16:07:16 +0530

    3&4

--- app.js
+++ app.js
@@ -1,19 +1,705 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id : "parent"},
-    [
-        React.createElement("div",{id : "child1"},[
-            React.createElement("h1",{id : "h1"},"hello world from h1"),
-            React.createElement("h2",{id : "h1"},"hello world from h2")
-        ])
-    ],
-    [
-        React.createElement("div",{id : "child2"},[
-            React.createElement("h1",{id : "h1"},"hello world from h1 child2"),
-            React.createElement("h2",{id : "h1"},"hello world from h2 child2")
-        ])
-    ]
-)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+const Header = () => {
+    return (
+        <>
+        <div className="header">
+            <img className="h-logo" alt="LOGO" src="https://www.reshot.com/preview-assets/icons/8U7VWRGFMS/food-8U7VWRGFMS.svg"/>
+            <div className="nav-items">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+              </ul>
+            </div>
+        </div>
+            </>
+    )
+}
+const RestaurantCard = ({ resObj }) =>{
+    return(
+        <>
+        <div className="card-container">
+            <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+resObj.info.cloudinaryImageId} alt="card-logo"/>
+            <h3>{resObj.info.name}</h3>
+            <h4>{resObj.info.avgRating}</h4>
+            <h4>{resObj.info.cuisines.join(",")}</h4>
+            <h4>{resObj.info.costForTwo}</h4>
+            <h4>{resObj.info.sla.deliveryTime} mins</h4>
+
+        </div></>
+    )
+}
+const Body = () => {
+ const resList= [
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "416860",
+"name": "KFC",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/17/8397b30c-3125-43cd-9cc9-5452c012aac9_416860.JPG",
+"locality": "Amravati Road",
+"areaName": "Gorantla",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Burgers",
+"Fast Food",
+"Rolls & Wraps"
+],
+"avgRating": 4.2,
+"parentId": "547",
+"avgRatingString": "4.2",
+"totalRatingsString": "5.0K+",
+"sla": {
+"deliveryTime": 46,
+"lastMileTravel": 14.5,
+"serviceability": "SERVICEABLE",
+"slaString": "45-50 mins",
+"lastMileTravelString": "14.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-20 23:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Rxawards/_CATEGORY-Burger.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Burger.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹79"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-f6e2bee3-a10d-4b47-8a1c-a4423ac34aac"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/vijayawada/kfc-amravati-road-gorantla-rest416860",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "995729",
+"name": "Chinese Wok",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/21/5b994ebc-703b-4224-885d-898dbda14465_995729.jpg",
+"locality": "Brodipet",
+"areaName": "Arundelpet",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Chinese",
+"Asian",
+"Tibetan",
+"Desserts"
+],
+"avgRating": 3.8,
+"parentId": "61955",
+"avgRatingString": "3.8",
+"totalRatingsString": "983",
+"sla": {
+"deliveryTime": 54,
+"lastMileTravel": 14.5,
+"serviceability": "SERVICEABLE",
+"slaString": "50-60 mins",
+"lastMileTravelString": "14.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-21 00:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹119"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-f6e2bee3-a10d-4b47-8a1c-a4423ac34aac"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/vijayawada/chinese-wok-brodipet-arundelpet-rest995729",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "995730",
+"name": "Big Bowl",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/21/abc8ee3d-69c7-44a5-bd85-dea1315376a3_995730.jpg",
+"locality": "Brodipet",
+"areaName": "Arundelpet",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Chinese",
+"Tibetan",
+"Desserts"
+],
+"avgRating": 3.7,
+"parentId": "434792",
+"avgRatingString": "3.7",
+"totalRatingsString": "685",
+"sla": {
+"deliveryTime": 57,
+"lastMileTravel": 14.5,
+"serviceability": "SERVICEABLE",
+"slaString": "55-65 mins",
+"lastMileTravelString": "14.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-21 00:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹129"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-f6e2bee3-a10d-4b47-8a1c-a4423ac34aac"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/vijayawada/big-bowl-brodipet-arundelpet-rest995730",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "712120",
+"name": "Prasad Fast Foods And Biryani",
+"cloudinaryImageId": "cf91af0ad62b20612678a67b324401b3",
+"locality": "Ganapathi Nagar",
+"areaName": "Mangalagiri",
+"costForTwo": "₹298 for two",
+"cuisines": [
+"Chinese",
+"Biryani",
+"Indian"
+],
+"avgRating": 4.3,
+"parentId": "425266",
+"avgRatingString": "4.3",
+"totalRatingsString": "1.5K+",
+"sla": {
+"deliveryTime": 30,
+"lastMileTravel": 9.5,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "9.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-20 22:30:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹89"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-f6e2bee3-a10d-4b47-8a1c-a4423ac34aac"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/vijayawada/prasad-fast-foods-and-biryani-ganapathi-nagar-mangalagiri-rest712120",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "1101972",
+"name": "Star Dhum Biryani",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/24/31fb705c-682d-4ab4-9bbe-7f03b6287b83_1101972.jpg",
+"locality": "Mangalagiri",
+"areaName": "Mangalagiri",
+"costForTwo": "₹300 for two",
+"cuisines": [
+"Biryani",
+"South Indian"
+],
+"avgRating": 4.1,
+"parentId": "643230",
+"avgRatingString": "4.1",
+"totalRatingsString": "161",
+"sla": {
+"deliveryTime": 28,
+"lastMileTravel": 8.1,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "8.1 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-20 23:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "20% OFF",
+"subHeader": "UPTO ₹50"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-f6e2bee3-a10d-4b47-8a1c-a4423ac34aac"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/vijayawada/star-dhum-biryani-mangalagiri-rest1101972",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "1236843",
+"name": "Little Village",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/18/c50b5ffc-1ead-4257-861b-430f94124144_1236843.jpg",
+"locality": "Mangalagiri",
+"areaName": "Mangalagiri",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Chinese",
+"Andhra",
+"Biryani",
+"South Indian",
+"Indian",
+"Beverages",
+"Grill",
+"Tandoor",
+"Fast Food"
+],
+"avgRating": 4.3,
+"parentId": "707595",
+"avgRatingString": "4.3",
+"totalRatingsString": "74",
+"sla": {
+"deliveryTime": 25,
+"lastMileTravel": 6.3,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "6.3 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-20 22:30:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-f6e2bee3-a10d-4b47-8a1c-a4423ac34aac"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/vijayawada/little-village-mangalagiri-rest1236843",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "1052682",
+"name": "Senapathi Military Hotel",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/2/25/6d055424-e5e7-43c2-88e8-ec6ed4470b06_1052682.jpg",
+"locality": "Mangalagiri",
+"areaName": "Mangalagiri",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Chinese",
+"Biryani",
+"Desserts",
+"South Indian"
+],
+"avgRating": 4.1,
+"parentId": "611970",
+"avgRatingString": "4.1",
+"totalRatingsString": "300",
+"sla": {
+"deliveryTime": 23,
+"lastMileTravel": 5,
+"serviceability": "SERVICEABLE",
+"slaString": "20-25 mins",
+"lastMileTravelString": "5.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-20 23:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-f6e2bee3-a10d-4b47-8a1c-a4423ac34aac"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/vijayawada/senapathi-military-hotel-mangalagiri-rest1052682",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "285440",
+"name": "Biryanis And More",
+"cloudinaryImageId": "wm8xnsxvd3g0hpuwpspt",
+"locality": "Chinakakani Village",
+"areaName": "Mangalagiri",
+"costForTwo": "₹300 for two",
+"cuisines": [
+"Biryani",
+"South Indian"
+],
+"avgRating": 4.3,
+"parentId": "2612",
+"avgRatingString": "4.3",
+"totalRatingsString": "5.5K+",
+"sla": {
+"deliveryTime": 31,
+"lastMileTravel": 5.6,
+"serviceability": "SERVICEABLE",
+"slaString": "30-35 mins",
+"lastMileTravelString": "5.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-20 22:45:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹149"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-f6e2bee3-a10d-4b47-8a1c-a4423ac34aac"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/vijayawada/biryanis-and-more-chinakakani-village-mangalagiri-rest285440",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+}
+]
+    return(
+        <>
+        <div className="body">
+            <p>Search bar</p>
+            <div className="res-container">
+           {resList.map((restaurent)=>{
+            return <RestaurantCard key={restaurent.info.id} resObj={restaurent} />
+           })}
+            
+            </div>
+        </div>
+        </>
+    )
+}
+const AppLayout = () => {
+    return(
+        <>
+        <div className="app">
+        <Header/>
+        <Body/>
+        </div>
+        </>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppLayout/>)
